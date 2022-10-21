21eb2a229d72f7aa7b3cb825d95cbf7f9c48a63d
lfybk39 <115467511+lfybk39@users.noreply.github.com>
2022-10-21 11:27:57 +0300

    хуява

--- HeroPecApp/MainForm.Designer.cs
+++ HeroPecApp/MainForm.Designer.cs
@@ -32,7 +32,10 @@ namespace HeroPecApp
             this.filesListView = new System.Windows.Forms.ListView();
             this.label4 = new System.Windows.Forms.Label();
             this.addFileButton = new System.Windows.Forms.Button();
-            this.saveButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.loadPictureBox = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.loadPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // filesListView
@@ -62,7 +65,7 @@ namespace HeroPecApp
             // 
             // addFileButton
             // 
-            this.addFileButton.Location = new System.Drawing.Point(81, 59);
+            this.addFileButton.Location = new System.Drawing.Point(81, 96);
             this.addFileButton.Name = "addFileButton";
             this.addFileButton.Size = new System.Drawing.Size(104, 23);
             this.addFileButton.TabIndex = 36;
@@ -70,15 +73,33 @@ namespace HeroPecApp
             this.addFileButton.UseVisualStyleBackColor = true;
             this.addFileButton.Click += new System.EventHandler(this.addFileButton_Click);
             // 
-            // saveButton
+            // deleteButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(81, 89);
-            this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(104, 23);
-            this.saveButton.TabIndex = 37;
-            this.saveButton.Text = "Сохранить";
-            this.saveButton.UseVisualStyleBackColor = true;
-            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            this.deleteButton.Location = new System.Drawing.Point(81, 125);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(104, 23);
+            this.deleteButton.TabIndex = 38;
+            this.deleteButton.Text = "Удaлить файл";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // loadPictureBox
+            // 
+            this.loadPictureBox.Image = global::HeroPecApp.Properties.Resources.logo;
+            this.loadPictureBox.Location = new System.Drawing.Point(428, 96);
+            this.loadPictureBox.Name = "loadPictureBox";
+            this.loadPictureBox.Size = new System.Drawing.Size(234, 259);
+            this.loadPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.loadPictureBox.TabIndex = 39;
+            this.loadPictureBox.TabStop = false;
+            this.loadPictureBox.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 200);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 40;
             // 
             // MainForm
             // 
@@ -86,13 +107,17 @@ namespace HeroPecApp
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(58)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.loadPictureBox);
+            this.Controls.Add(this.deleteButton);
             this.Controls.Add(this.addFileButton);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.filesListView);
             this.Name = "MainForm";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.loadPictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,7 +128,9 @@ namespace HeroPecApp
         private System.Windows.Forms.ListView filesListView;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button addFileButton;
-        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.PictureBox loadPictureBox;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- HeroPecApp/MainForm.cs
+++ HeroPecApp/MainForm.cs
@@ -10,16 +10,56 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Compression;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace HeroPecApp
 {
     public partial class MainForm : Form
     {
-        private string userZip = $"{Environment.CurrentDirectory}\\TempData\\{Properties.Settings.Default.CurrentUserLogin}.zip";
+        private static string currentUserLogin = Core.Context.Users.FirstOrDefault(u => Properties.Settings.Default.CurrentUserLogin.Contains("@") ?
+        u.Email == Properties.Settings.Default.CurrentUserLogin
+        : u.Login == Properties.Settings.Default.CurrentUserLogin).Login;
+        private string currentUserPassword = GetHash(Properties.Settings.Default.CurrentUserPassword);
+        private string userZip = $"{Environment.CurrentDirectory}\\TempData\\{currentUserLogin}.zip";
         private string heroZip = $"{Environment.CurrentDirectory}\\DataFiles\\HeroData.zip";
+
+        public static string GetHash(string input)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            return Convert.ToBase64String(hash);
+        }
+
+        private void AddFile()
+        {
+            loadPictureBox.Visible = true;
+            var addFile = new OpenFileDialog();
+            if (addFile.ShowDialog() == DialogResult.OK)
+            {
+                using (var zip = new ZipFile(userZip))
+                {
+                    zip.AddFile(addFile.FileName, "");
+                    zip.Save(userZip);
+                }
+            }
+            loadPictureBox.Visible = false;
+        }
+
+        private void SaveData()
+        {
+            using (var zip = new ZipFile(heroZip))
+            {
+                zip.Password = currentUserPassword;
+                zip.RemoveEntry($"{currentUserLogin}.zip");
+                zip.AddFile(userZip, "");
+                zip.Save(heroZip);
+            }
+        }
 
         private void FillListView()
         {
+            loadPictureBox.Visible = true;
             filesListView.Items.Clear();
             using (var Zip = new ZipFile(userZip))
             {
@@ -28,6 +68,7 @@ namespace HeroPecApp
                     filesListView.Items.Add(entry.FileName);
                 }
             }
+            loadPictureBox.Visible = false;
         }
 
         public MainForm()
@@ -49,7 +90,7 @@ namespace HeroPecApp
                 {
                     zip.CompressionLevel = Ionic.Zlib.CompressionLevel.BestCompression; // Задаем максимальную степень сжатия 
                     zip.Save(userZip);
-                    zip.Password = Properties.Settings.Default.CurrentUserPassword;
+                    zip.Password = currentUserPassword;
                     zip.AddFile(userZip, "");
                     zip.Save($"{Environment.CurrentDirectory}\\DataFiles\\HeroData.zip");
                 }
@@ -57,46 +98,43 @@ namespace HeroPecApp
 
             using (ZipFile zip = new ZipFile(heroZip))
             {
-                zip.Password = Properties.Settings.Default.CurrentUserPassword;
+                zip.Password = currentUserPassword;
                 if (File.Exists(userZip))
                 {
                     File.Delete(userZip);
                 }
-                zip.Entries.FirstOrDefault(en => en.FileName == $"{Properties.Settings.Default.CurrentUserLogin}.zip").Extract($"{Environment.CurrentDirectory}\\TempData");
+                zip.Entries.FirstOrDefault(en => en.FileName == $"{currentUserLogin}.zip").Extract($"{Environment.CurrentDirectory}\\TempData");
             }
             FillListView();
+            textBox1.Text = currentUserPassword;
         }
 
         private void addFileButton_Click(object sender, EventArgs e)
         {
-            var addFile = new OpenFileDialog();
-            if(addFile.ShowDialog() == DialogResult.OK)
-            {
-                using (var zip = new ZipFile(userZip))
-                {
-                    zip.AddFile(addFile.FileName, "");
-                    zip.Save(userZip);
-                }
-            }
+            AddFile();
             FillListView();
         }
 
-        private void saveButton_Click(object sender, EventArgs e)
+        private void deleteButton_Click(object sender, EventArgs e)
         {
-            try
+            if (filesListView.SelectedItems.Count != 0)
             {
-                using (var zip = new ZipFile(heroZip))
+                using (var zip = new ZipFile(userZip))
                 {
-                    zip.Password = Properties.Settings.Default.CurrentUserPassword;
-                    zip.RemoveEntry($"{Properties.Settings.Default.CurrentUserLogin}.zip");
-                    zip.AddFile(userZip, "");
-                    zip.Save(heroZip);
+                    foreach (ListViewItem item in filesListView.SelectedItems)
+                    {
+                        zip.RemoveEntry(item.Text);
+                        zip.Save(userZip);
+                    }
                 }
+                FillListView();
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveData();
+            File.Delete(userZip);
         }
     }
 }
